a5fd10b4ffc2b8b7c9a2483ebc16b414e08a23a1
Den <bk6om1986@gmail.com>
2022-03-19 12:26:33 +0300

    Junit4 check again

--- src/test/java/ru/netology/service/CashbackHackerTest.java
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -15,7 +15,7 @@ public class CashbackHackerTest {
         int actual = service.remain(amount);
         int expected = 1000;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 
@@ -26,7 +26,7 @@ public class CashbackHackerTest {
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 
@@ -37,7 +37,7 @@ public class CashbackHackerTest {
         int actual = service.remain(amount);
         int expected = 999;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -47,7 +47,7 @@ public class CashbackHackerTest {
         int actual = service.remain(amount);
         int expected = 1;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 
